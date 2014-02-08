7bb62773cf7bb00e6eaf0359772fd70fcf5e9dbb
Shayan Sayahi <ss.synix@gmail.com>
2014-02-08 17:45:32 -0600

    Another update to DBMS.h

--- DBMS-Engine/DBMS.h
+++ DBMS-Engine/DBMS.h
@@ -17,14 +17,14 @@ class DBMS
 
 private:
 
-    unordered_map<string, Table*> relations; 	// Constructor....
+    unordered_map<string, Table> relations; 	// Constructor....
 
 public:
 
     DBMS();
     ~DBMS();
 
-    Table* open_cmd(string name);
+    Table open_cmd(string name);
     void close_cmd(string name);
     void write_cmd(string name);
     void exit_cmd();
@@ -32,15 +32,15 @@ public:
     void create_cmd(string name, Type[] attributes, string[] primaryKey);
     void update_cmd(string name, tuple<string, string> fieldsToUpdate, Condition cond); // tuple<attributeName, Value>
     void insert_cmd(string name, string[] values);
-    void insert_cmd(string name, Table* fromRelation);
+    void insert_cmd(string name, Table fromRelation);
     void delete_cmd(string name, Condition cond);
 
-    Table* selection();
-    Table* projection();
-    void renaming();
-    Table* union_();
-    Table* difference();
-    Table* cross_product();
-    Table* natural_join();
+    Table selection(Condition cond, Table relation);
+    Table projection(string[] attributes, Table relation);
+    Table renaming(string[] attributes, Table relation);
+    Table union_(Table rel1, Table rel2);
+    Table difference(Table rel1, Table rel2);
+    Table cross_product(Table rel1, Table rel2);
+    Table natural_join(Table rel1, Table rel2);
 
 };
