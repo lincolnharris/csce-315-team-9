0f67b61bf5ff6b16017bba1958237870cd38b997
Shayan Sayahi <ss.synix@gmail.com>
2014-02-08 15:47:23 -0600

    test 2

---
+++ DBMS-Engine/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Feb 8, 2014
+ *      Author: Synix
+ */
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	cout << "hello" << endl;
+	return 1;
+}
