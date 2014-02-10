6c65a05e8cbbb631cca0487047d7e970a62cba5b
lincolnharris <lincolnharris@tamu.edu>
2014-02-09 20:49:46 -0600

    Update DBMS.cpp

--- DBMS-Engine/DBMS.cpp
+++ DBMS-Engine/DBMS.cpp
@@ -1,144 +1,58 @@
 // Class Includes
 #include "DBMS.h"
-#include "Comparison.h"
+
 
 using namespace std;
 
-
-/******************** Constructor / Destructor *******************************/
-DBMS::DBMS()
-{
-	// Nothing
-}
-
-
-DBMS::~DBMS()
-{
-	// Nothing
-}
-
-
-
-/******************** DBMS General Commands *******************************/
-//Table DBMS::open_cmd(string name)
-//{
-//
-//
-//}
-//
-//
-//void DBMS::close_cmd(string name)
-//{
-//
-//
-//}
-//
-//
-//void DBMS::write_cmd(string name)
-//{
-//
-//
-//}
-//
-//
-//void DBMS::exit_cmd()
-//{
-//
-//
-//}
-//
-//
-//void DBMS::show_cmd(string name)
-//{
-//
-//
-//}
-//
-
-
-
 /******************** Table Manipulation *******************************/
 void DBMS::create_cmd(string name, vector<Type> attributes, vector<string> primaryKey)
-{
-	// Lincoln
+{// Lincoln
+	Table table;
+	table.attributeMap = attributes;
+	table.keys = primaryKey;
+	relations.insert( {name , table} );
 }
 
 
-void DBMS::delete_cmd(Table table, Comparison cond)
-{
-	// Lincoln
+void DBMS::delete_cmd(Table table, Condition cond)
+{// Lincoln
+	for( list<vector<string>>::iterator it = table.rows.begin(); it != table.rows.end(); ++it){
+		if ( cond(*it) ) table.rows.erase(it);
+	}	
 }
-
-
-
-void DBMS::update_cmd(Table table, vector<pair<int, string>> fieldsToUpdate, Comparison cond) // tuple<attributeName, Value>
-{
-	// Dmitry
-}
-
-
-
-void DBMS::insert_cmd(Table table, vector<string> values)
-{
-	// Shayan
-}
-
-
-
-void DBMS::insert_cmd(Table table, Table fromRelation)
-{
-	// Shayan
-}
-
-
-
 
 /******************** Table Logic Algebra *******************************/
-Table DBMS::selection(Comparison cond, Table relation)
-{
-	// Lincoln
+Table DBMS::selection(Condition cond, Table table)
+{// Lincoln
+	Table selected;
+	selected.attributeMap = table.attributeMap;
+	selected.keys = table.keys;
+	for( list<vector<string>>::iterator it = table.rows.begin(); it != table.rows.end(); ++it){
+		if ( cond(*it) ) selected.rows.push_back(*it);	
+	}
 }
 
 
 
-Table DBMS::projection(vector<string> attributes, Table relation)
-{
-	// Lincoln
+Table DBMS::projection(vector<string> attributes, Table table)
+{// Lincoln
+	Table proj;
+	proj.attributeMap = table.attributeMap;
+	proj.keys = table.keys;
+	proj.rows = table.rows;
+	int numOfColumns = proj.attributeMap.size();
+	int numOfAtts = attributes.size();
+	for(int i=0; i<numOfColumns; ++i){
+		bool found = false;
+		for(int j=0; (j<numOfAtts || !(found) ); ++j){
+			if( (proj.attributeMap)[i].attrb_name == attributes[j] ) found = true;
+		}
+		if( !(found) ){
+			for( vector<string> row : proj.rows){
+				row.erase(row.begin() + i);
+			}
+			proj.attributeMap.erase(proj.attributeMap.begin()+i);
+			--i;
+		}
+	}
 }
-
-
-
-Table DBMS::renaming(vector<string> attributes, Table relation)
-{
-
-}
-
-
-
-Table DBMS::union_(Table rel1, Table rel2)
-{
-	// Dmitry
-}
-
-
-
-Table DBMS::difference(Table rel1, Table rel2)
-{
-	// Dmitry
-}
-
-
-
-Table DBMS::cross_product(Table rel1, Table rel2)
-{
-	// Shayan
-}
-
-
-
-Table DBMS::natural_join(Table rel1, Table rel2)
-{
-	// Dmitry
-}
-
-
