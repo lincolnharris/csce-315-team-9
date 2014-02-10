7dc7c2d128607822633b5eeaca1b5e1b2cf06e0a
Dmitry Ionan <dmitrioligy@gmail.com>
2014-02-10 16:32:06 -0600

    Shayan + Dmitry fused

--- DBMS-Engine/DBMS.cpp
+++ DBMS-Engine/DBMS.cpp
@@ -88,6 +88,26 @@ void DBMS::update_cmd(Table table, vector<pair<int, string>> fieldsToUpdate, Com
 void DBMS::insert_cmd(Table table, vector<string> values)
 {
 	// Shayan
+
+	table.rows.push_back(vector<string>(table.attributeMap.size()));
+
+    if(values.size() != table.attributeMap.size())
+        throw "Not enough attributes were given.";
+    for(auto& pair : table.attributeMap)
+    {
+        Type& t = pair.second; // pair<name, (index, type)>
+
+        // Type checking:
+        if(t.type == -1)
+            try { stoi(values[t.index]); }
+            catch (...)
+            {
+                table.rows.pop_back();
+                throw "Wrong type: " + values[t.index];
+            }
+
+        table.rows.back()[t.index] = values[t.index]; // The actual assignment
+    }
 }
 
 
@@ -95,6 +115,14 @@ void DBMS::insert_cmd(Table table, vector<string> values)
 void DBMS::insert_cmd(Table table, Table fromRelation)
 {
 	// Shayan
+
+	 if(!is_union_compatible(table, fromRelation))
+        throw "Not union compatible.";
+
+    // Just making sure they're the same; they could be different in attribute ordering.
+    table.attributeMap = fromRelation.attributeMap;
+
+    table.rows.insert(table.rows.end(), fromRelation.rows.begin(), fromRelation.rows.end());
 }
 
 
@@ -285,6 +313,26 @@ Table DBMS::difference(Table rel1, Table rel2)
 Table DBMS::cross_product(Table rel1, Table rel2)
 {
 	// Shayan
+
+	Table xprod;
+
+    // Merging the attributeMaps:
+    xprod.attributeMap = t1.attributeMap;
+    for(auto pair : t2.attributeMap) // pair<name, (index, type)
+    {
+        pair.second.index += t1.attributeMap.size();
+        xprod.attributeMap["_" + pair.first] = pair.second; // Hyphenated to avoid repeated names
+    }
+
+    // Creating the cross-producted rows:
+    for(auto& row1 : t1.rows)
+        for(auto& row2 : t2.rows)
+        {
+            xprod.rows.push_back(row1); // TODO Consider making it faster by .reserve() beforehand.
+            xprod.rows.back().insert(xprod.rows.back().end(), row2.begin(), row2.end());
+        }
+
+    return xprod;
 }
 
 
@@ -295,3 +343,16 @@ Table DBMS::natural_join(Table rel1, Table rel2)
 }
 
 
+bool is_union_compatible(const Table& t1, const Table t2)
+{
+    if(t1.attributeMap.size() != t2.attributeMap.size())
+        return false;
+
+    // Do both have the same type of attributes?
+    string name;
+    for(auto& pair : t1.attributeMap) // pair<name, Type>
+        if(t2.attributeMap.find(pair.first) == t2.attributeMap.end())
+            return false;
+
+    return true;
+}
