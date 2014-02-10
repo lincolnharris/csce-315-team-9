e5aeecd7b23d798da726b6a1df03d46be8883ec0
Dmitry Ionan <dmitrioligy@gmail.com>
2014-02-10 16:45:55 -0600

    fixed merge text

--- DBMS-Engine/DBMS.h
+++ DBMS-Engine/DBMS.h
@@ -9,52 +9,12 @@
 
 using namespace std;
 
-<<<<<<< HEAD
 
 class Comparison; // TODO how to handle conditions? Combining Function Objects returning bool?
-=======
-class Comparison;
-// TODO how to handle conditions? Combining Function Objects returning bool?
->>>>>>> 7f43335645aabef52e63c6659e55f1e0b138a07d
 
 class DBMS
 {
 
-<<<<<<< HEAD
-	private:
-
-	    unordered_map<string, Table> _relation; 	// Constructor....
-
-	public:
-
-	    DBMS();
-	    ~DBMS();
-
-
-	    Table open_cmd(string name);
-	    void close_cmd(string name);
-	    void write_cmd(string name);
-	    void exit_cmd();
-	    void show_cmd(string name);
-
-
-
-	    void create_cmd(string name, Type attributes[], string primaryKey[]);
-	    void delete_cmd(string name, Condition cond);
-
-		// tuple<attributeName, Value>
-	    void update_cmd(Table table, vector< pair<int, string> > fieldsToUpdate, Comparison cond) ; 
-	    void insert_cmd(Table table, string values[]);
-	    void insert_cmd(Table table, Table fromRelation);
-
-	    Table selection(Condition cond, Table relation);
-	    Table projection(string attributes[], Table relation);
-	    Table renaming(string attributes[], Table relation);
-	    Table union_(Table rel1, Table rel2);
-	    Table difference(Table rel1, Table rel2);
-	    Table cross_product(Table rel1, Table rel2);
-	    Table natural_join(Table rel1, Table rel2);
-=======
 private:
 
     unordered_map<string, Table> relations; 	// Constructor....
@@ -82,6 +42,5 @@ public:
     Table difference(Table rel1, Table rel2);
     Table cross_product(Table rel1, Table rel2);
     Table natural_join(Table rel1, Table rel2);
->>>>>>> 7f43335645aabef52e63c6659e55f1e0b138a07d
 
 };
