44535180dd7c1d9cd01bf624f77cf02c50a1feb0
Dmitry Ionan <dmitrioligy@gmail.com>
2014-02-10 16:33:39 -0600

    Synced Table.cpp and Table.h to be included in Dmitry Directory

---
+++ DBMS-Engine/Table.cpp
@@ -0,0 +1,22 @@
+/*
+ * Table.cpp
+ *
+ *  Created on: Feb 8, 2014
+ *      Author: Synix
+ */
+
+#include "Table.h"
+
+
+Table::Table()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+
+Table::~Table()
+{
+    // TODO Auto-generated destructor stub
+}
+
--- DBMS-Engine/Table.h
+++ DBMS-Engine/Table.h
@@ -20,7 +20,7 @@ struct Type
     int   index;
     short type;     // -1 == int, anything positive is a string of that length
 
-    Type() {}
+    Type() = default;
     Type(int index, short type) : index(index), type(type) {}
 };
 
@@ -29,13 +29,11 @@ class Table
 	
 	public:
 
-		// vector <attribute indx> to <column index, data type>
-	    unordered_map<string, Type> attributeMap;
+	    unordered_map<string, Type> attributeMap;   // Mapping <attribute name> to <column index, data type>
 
-		// The table is stored as a linked list of rows
-	    // each row is an array of strings
-	    // each string holds the value of its respective column
-	    list<vector<string>>        rows;           
+	    list<vector<string>>        rows;           // The table is stored as a linked list of rows
+	                                                // each row is an array of strings
+	                                                // each string holds the value of its respective column
 
 
 	    // Helper functions for the future!
