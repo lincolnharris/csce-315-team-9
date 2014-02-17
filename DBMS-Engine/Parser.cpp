714aa6aa8cf05bc98b3460e720309e36a162a387
Dmitry Ionan <dmitrioligy@gmail.com>
2014-02-16 21:44:35 -0600

    no message

--- DBMS-Engine/Parser.cpp
+++ DBMS-Engine/Parser.cpp
@@ -13,7 +13,7 @@ ParsedResult	Parser::query (vector<Token> tokens)
 }
 
 
-ParsedResult	Parser::relation_name (vector<Token> tokens)
+ParsedResult<string>	Parser::relation_name (vector<Token> tokens)
 {
 
 }
@@ -53,46 +53,46 @@ ParsedResult	Parser::atomic_expr (vector<Token> tokens)
 
 
 
-ParsedResult	Parser::selection (vector<Token> tokens)
+ParsedResult<Table>	Parser::selection (vector<Token> tokens)
 {
 
 }
 
 
-ParsedResult	Parser::condition (vector<Token> tokens)
-{
-
-}
-
-
-
-ParsedResult	Parser::conjunction (vector<Token> tokens)
+ParsedResult<conditinon>	Parser::condition (vector<Token> tokens)
 {
 
 }
 
 
 
-ParsedResult	Parser::comparison (vector<Token> tokens)
-{
-
-}
-
-
-ParsedResult	Parser::op (vector<Token> tokens)
-{
-
-}
-
-
-ParsedResult	Parser::operand (vector<Token> tokens)
+ParsedResult<Table>	Parser::conjunction (vector<Token> tokens)
 {
 
 }
 
 
 
-ParsedResult	Parser::attribute_name (vector<Token> tokens)
+ParsedResult<Comparison>	Parser::comparison (vector<Token> tokens)
+{
+
+}
+
+
+ParsedResult<char>	Parser::op (vector<Token> tokens)
+{
+
+}
+
+
+ParsedResult<string>	Parser::operand (vector<Token> tokens)
+{
+
+}
+
+
+
+ParsedResult<sring>	Parser::attribute_name (vector<Token> tokens)
 {
 
 }
@@ -106,44 +106,44 @@ ParsedResult<string>	Parser::literal (vector<Token> tokens)
 
 
 
-ParsedResult	Parser::projection (vector<Token> tokens)
+ParsedResult<Table>	Parser::projection (vector<Token> tokens)
 {
 
 }
 
 
 
-ParsedResult	Parser::attribute_list (vector<Token> tokens)
+ParsedResult<string>	Parser::attribute_list (vector<Token> tokens)
 {
 
 }
 
 
-ParsedResult	Parser::renaming (vector<Token> tokens)
+ParsedResult<Table>	Parser::renaming (vector<Token> tokens)
 {
 
 }
 
 
-ParsedResult	Parser::union (vector<Token> tokens)
+ParsedResult<Table>	Parser::union (vector<Token> tokens)
 {
 
 }
 
 
-ParsedResult	Parser::difference (vector<Token> tokens)
+ParsedResult<Table>	Parser::difference (vector<Token> tokens)
 {
 
 }
 
 
-ParsedResult	Parser::product (vector<Token> tokens)
+ParsedResult<Table>	Parser::product (vector<Token> tokens)
 {
 
 }
 
 
-ParsedResult	Parser::natural_join (vector<Token> tokens)
+ParsedResult<Table>	Parser::natural_join (vector<Token> tokens)
 {
 
 }
