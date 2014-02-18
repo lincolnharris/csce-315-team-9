941577aa2949af2542a3e1c421d4d69de49e3bbf
lincolnharris <lincolnharris@tamu.edu>
2014-02-18 13:59:43 -0600

    Update DBMS.cpp

--- DBMS-Engine/DBMS.cpp
+++ DBMS-Engine/DBMS.cpp
@@ -4,64 +4,162 @@
 
 using namespace std;
 
-/******************** Table Manipulation *******************************/
-void DBMS::create_cmd(string name, vector< pair<string,Type> > attributes, vector<string> primaryKey)
-{// Lincoln
-	Table table;
-	int size = attributes.size();
-	for(int i=0; i<size; ++i){	//go through each attribute and add it to the attribute map
-		pair<string,Type> temp(attributes[i]);
-		table.attributeMap.insert( temp );
-	}
-	table.keys = primaryKey;
-	relation[name] = table;
+Table DBMS::open_cmd(string name){
+    //initialize some basic data
+    fstream file;
+    file.open(name+fileType);
+    if( ! file.is_open() ) return Table(); //could not open the file
+    vector< vector<string> > rows;
+    
+    //get all the rows
+    string line;
+    int counter = 0;
+    while ( getline(file, line) ) {
+        rows.push_back(vector<string>());
+        stringstream ss;
+        ss << line;
+        string word;
+        while ( ss >> word ){
+            rows[counter].push_back(word);
+        }
+        ++counter;
+    }
+    
+    //get the attributes and their types
+    vector<pair<string, Type> > attributes;
+    int size = rows[0].size();
+    for(int i=0; i < size; i += 2){
+        string type = (rows[0])[i+1];
+        Type t;
+        t.index = i/2;
+        if( type.substr(0,7) == VARCHAR ) {
+            int j = VARCHAR.size() + 1;
+            string c = "";
+            while( type[j] != ')' ){
+                c += type[j];
+            }
+            t.type = atoi(c.c_str());
+        }
+        else {
+            t.type = -1;
+        }
+        pair<string, Type> temp( (rows[0])[i] , t );
+        attributes.push_back( temp );
+        file.close();
+    }
+    
+    //create the table
+    create_cmd(name, attributes, vector<string>());
+    //get a pointer to it
+    unordered_map<string,Table>::iterator it = relations.find(name);
+    //insert all of the rows into the table
+    for(int i=1; i<rows.size(); ++i){
+        insert_cmd( it->second, rows[i] );
+    }
+
+    return it->second;
 }
 
+void DBMS::close_cmd(string name){
+    //initialize some basic data
+    unordered_map<string,Table>::iterator it = relations.find(name);
+    if( it == relations.end() ) return; //could not find the relation
+    ofstream file;
+    file.open(name+fileType);
+    if( ! file.is_open() ) return; //could not open the file
 
-void DBMS::delete_cmd(Table table, Comparison cond)
-{// Lincoln
-	for( auto it = table.rows.begin(); it != table.rows.end(); ++it){//go through each row
-		if ( cond(*it) ) table.rows.erase(it);//if the row passes the condition, delete the row
-	}	
+    //get the first line (attributes)
+    vector<string> attributes;
+    vector<int> sizes;
+    Table* ptable = &(it->second);
+    for(auto& attribute : ptable->attributeMap ){
+        attributes.push_back(attribute.first);
+        int size = attribute.second.type;
+        if(size == -1){
+            attributes.push_back(INTEGER);
+            sizes.push_back(15);
+        }
+        else{
+            char s[sizeof(int)];
+            itoa(size,s,10);
+            attributes.push_back(VARCHAR+"("+ s + ")");
+            sizes.push_back(size+10);
+        }
+    }
+    
+    //add to file
+    int numOfColumns = sizes.size();
+    for(int i=0; i<numOfColumns*2; i+=2){
+        string temp = attributes[i] + " " + attributes[i+1];
+        while(temp.size() <= sizes[i]){
+            temp += " ";
+        }
+        file << temp;
+    }
+    file << '\n';
+
+    //add the rows to the file
+    for(vector<string> row : ptable->rows){
+        for(int i=0; i<numOfColumns; ++i){
+            string line = row[i];
+            while(line.size() < sizes[i]) line += " ";
+            file << line;
+        }
+        file << '\n';
+    }
+    file.close();
 }
 
-/******************** Table Logic Algebra *******************************/
-Table DBMS::selection(Comparison cond, Table table)
-{// Lincoln
-	Table selected;
-	selected.attributeMap = table.attributeMap;
-	for( auto it = table.rows.begin(); it != table.rows.end(); ++it){ //go through each row
-		if ( cond(*it) ) selected.rows.push_back(*it);	//if the row passes the condition add it to the new table
-	}
-	return selected;
+void DBMS::write_cmd(string name){
+    ofstream file;
+    file.open(name+fileType);
+    file.close();
+    close_cmd(name);
 }
 
+void DBMS::exit_cmd(){ ~DBMS(); }
 
+void DBMS::show_cmd(Table table){
+    //initialize some basic data
+    unordered_map<string,Table>* it = &(table);
 
-Table DBMS::projection(vector<string> attributes, Table table)
-{// Lincoln
-	Table proj;
-	int numOfAtts = attributes.size();
-	int numOfRows = table.rows.size();
-	list<vector<strings> > lines;	//hold the rows for the project for now
-	for(int i=0; i<numOfRows; ++i;){//set up all the rows for the projection, but leave them empty for now
-		vector<string> temp;
-		lines.push_back(temp);
-	}
-	for(int i=0; i<numOfAtts; ++i){	//go through all the wanted attributes
-		auto it = table.find (attributes[i]); //get an iterator to the Type of the attribute
-		auto itA = lines.begin(); //iterator to the row where the attribute needs to be inserted
-		auto itB = table.rows.begin(); //iterator to the attribute that needs to be inserted into new table
-		for(int j=0; j<numOfRows; ++j){	//go through every row
-			itA->push_back( (*itB)[ (it->second).index ] ); //add the correct string, based on the types index, from the old table to the projection
-			++itA;	//go to next line for the projection
-			++itB;	//go to next line for the old table
-		}
-		Type temp = it->second;
-		temp.index = i;	//change the index of the type to the new index
-		proj.attributeMap[attributes[i]] = temp; //add the attribute to the projection
-		
-	}
-	proj.rows = lines;	//add all of the rows to the project
-	return proj;
+    //get the first line (attributes)
+    vector<string> attributes;
+    vector<int> sizes;
+    Table* ptable = &(it->second);
+    for(auto& attribute : ptable->attributeMap ){
+        attributes.push_back(attribute.first);
+        int size = attribute.second.type;
+        if(size == -1){
+            attributes.push_back(INTEGER);
+            sizes.push_back(15);
+        }
+        else{
+            char s[sizeof(int)];
+            itoa(size,s,10);
+            attributes.push_back(VARCHAR+"("+ s + ")");
+            sizes.push_back(size+10);
+        }
+    }
+
+    //write to screen
+    int numOfColumns = sizes.size();
+    for(int i=0; i<numOfColumns*2; i+=2){
+        string temp = attributes[i] + " " + attributes[i+1];
+        while(temp.size() <= sizes[i]){
+            temp += " ";
+        }
+        cout << temp;
+    }
+    cout << '\n';
+
+    //print the rows
+    for(vector<string> row : ptable->rows){
+        for(int i=0; i<numOfColumns; ++i){
+            string line = row[i];
+            while(line.size() < sizes[i]) line += " ";
+            cout << line;
+        }
+        cout << '\n';
+    }
 }
