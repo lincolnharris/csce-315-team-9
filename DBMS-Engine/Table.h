1aa6a12de14e057f98fa3daeb80a95b9d9cb150f
lincolnharris <lincolnharris@tamu.edu>
2014-02-24 13:00:25 -0600

    Update Table.h

--- DBMS-Engine/Table.h
+++ DBMS-Engine/Table.h
@@ -1,9 +1,9 @@
 /*
- * Table.h
- *
- *  Created on: Feb 8, 2014
- *      Author: Synix
- */
+* Table.h
+*
+*  Created on: Feb 8, 2014
+*      Author: Synix
+*/
 
 #pragma once
 
@@ -12,36 +12,47 @@
 #include <list>
 #include <vector>
 
-using namespace std;
+using std::string;
+using std::vector;
+using std::unordered_map;
+using std::list;
 
 
 struct Type
 {
-    int   index;
-    short type;     // -1 == int, anything positive is a string of that length
+	int   index;
+	short type;     // -1 == int, anything positive is a string of that length
 
-    Type() {}
-    Type(int index, short type) : index(index), type(type) {}
+	Type() {};
+	Type(int index, short type) : index(index), type(type) {}
+
+	bool operator==(const Type& t)
+	{
+		return (index == t.index && type == t.type);
+	}
+	bool operator!=(const Type& t) { return !((*this) == t); }
 };
 
 class Table
 {
-	
-	public:
 
-		vector<string> keys;
-	    unordered_map<string, Type> attributeMap;   // Mapping <attribute name> to <column index, data type>
+public:
 
-	    list<vector<string>>        rows;           // The table is stored as a linked list of rows
-	                                                // each row is an array of strings
-	                                                // each string holds the value of its respective column
+	unordered_map<string, Type> attributeMap;   // Mapping <attribute name> to <column index, data type>
+	vector<string>              keys;
 
+	list<vector<string>>        rows;           // The table is stored as a linked list of rows
+	// each row is an array of strings
+	// each string holds the value of its respective column
 
-	    // Helper functions for the future!
-	    int getInt();
-	    string getString();
+	bool operator==(const Table& t);
+	bool operator!=(const Table& t);
 
-	    Table();
-	    ~Table();
+	// Helper functions for the future!
+	int getInt();
+	string getString();
+
+	Table();
+	~Table();
 };
 
